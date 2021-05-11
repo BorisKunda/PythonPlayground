c9e4b41925b89fcebaf38533966d0a457a259c43
Boris Kunda <34305099+BorisKunda@users.noreply.github.com>
2021-05-11 17:26:20 +0300

    python avengers

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello World")
+class Avenger:
+    def __init__(self, name, attack):
+        self.name = name + " ---- "
+        self.attack = attack
+
+
+# -----
+
+print("AVENGERS ASSEMBLE !!!")
+
+a1 = Avenger("Spiderman", "SHOOTS WEBS")
+a2 = Avenger("Punisher", "LAUNCHES ROCKET")
+a3 = Avenger("Thor", "THROWS HAMMER")
+
+avengers = [a1, a2, a3]
+
+for hero in avengers:
+    print(hero.name + hero.attack)
