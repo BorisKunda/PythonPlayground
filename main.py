22823a7d2d935185782e3c1325b70279be871f80
Boris Kunda <34305099+BorisKunda@users.noreply.github.com>
2021-05-17 18:40:14 +0300

    commit 5

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-# data
 from typing import List, Set
+
+
+# classes
+
+class SampleClass:
+    def __init__(self, x):
+        self.x = x
+
+
+# vars
 
 alpha = "Alpha"
 beta = "Beta"
+
+s1 = SampleClass(1)
+s2 = SampleClass(2)
 
 plist: List[int] = [0, 1, 2, 3]
 pSet: Set[str] = {"apple", "banana", "cherry"}
@@ -19,11 +31,8 @@ def b(): print("B")
 
 def c(): pass
 
-
 # ----  ----
 # ----  ----
 # ---- EXECUTION ----
 # ----  ----
 # ----  ----
-
-
