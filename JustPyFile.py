16165d85a30b6fc2fe010e3de496b1ca573e6f85
Boris Kunda <34305099+BorisKunda@users.noreply.github.com>
2021-05-19 12:25:15 +0300

    added another file

---
+++ JustPyFile.py
@@ -0,0 +1,33 @@
+def perform_a(): print("a")
+
+
+def perform_b(): print("b")
+
+
+def perform_c(): print("c")
+
+
+def perform_d(): print("d")
+
+
+def perform_e(): print("e")
+
+
+def perform_f():
+    print("f")
+
+
+def perform_g(a, b):
+    if a > b:
+        perform_a()
+    else:
+        perform_b()
+        perform_c()
+
+
+
+
+
+
+
+
